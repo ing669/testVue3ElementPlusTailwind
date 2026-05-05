7fed6216422bc26f2ffe751098244b32e77a16ac
ing669 <ing151566@gmail.com>
2026-05-06 04:33:10 +0800

    初始化,tailwindcss版本3配置成功

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@ const userStore = useUserStore();
 </script>
 
 <template>
-    <div class="text-xs text-red-500"> Hello, World! </div>
+    <div class="text-red-500 text-3xl"> Hello, World! </div>
     <div class="text-xs text-red-500"> {{ userStore.token }} </div>
     <el-button type="primary"><el-icon><CirclePlus /></el-icon>添加</el-button>
 </template>
